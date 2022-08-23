639e16ac5968b426e20efaf525d67ba0d2ef7e71
Peyton Bechard <pbechard@my.yorku.ca>
2022-08-23 08:46:19 +0800

    Adds hr tag dividers in document preview and export

--- src/components/PreviewModal.js
+++ src/components/PreviewModal.js
@@ -3,13 +3,27 @@ import { Button, Modal } from 'react-bootstrap';
 import { Preview } from 'react-html2pdf';
 import uniqid from 'uniqid';
 
+// FIXME: Make modal responsive for mobile devices
+
 function PreviewModal(props) {
   const formattedDocument = (
     // TODO: Add styling and parsing here for export formatting
-    <div style={{ fontSize: '12px', padding: '20px' }}>
+    <div style={{ fontSize: '12px', padding: '20px' }} className='row'>
       {Object.entries(props.doc).map((value) => {
         return (
-          <div key={uniqid()}>
+          <div
+            key={uniqid()}
+            className={
+              value[0].split('-')[1] === 'completed' ? 'col-12' : 'col-6'
+            }
+          >
+            {value[0].split('-')[0] === 'position' && <hr />}
+            {value[0].split('-')[0] === 'company' &&
+              value[0].split('-').length === 2 && <hr />}
+            {value[0].split('-')[0] === 'institution' &&
+              value[0].split('-').length === 2 && <hr />}
+            {value[0].split('-')[0] === 'degree' && <hr />}
+            {value[0].split('-')[0] === 'other' && <hr />}
             {value[0].toUpperCase()}: {value[1]}
           </div>
         );
